af0d0615a8f15dbe91eabd593215c5bf286c7001
Renze Yu <renzeyu@microsoft.com>
2016-12-13 16:21:30 +0800

    prepare test case for multiple type param

--- spec/fixtures/MyClass.js
+++ spec/fixtures/MyClass.js
@@ -4,4 +4,22 @@
  * @classdesc This is a description of the MyClass class.
  */
 function MyClass() {
+  /**
+  * @param {string} somebody Somebody's name.
+  */
+  function sayHello(somebody) {
+    alert('Hello ' + somebody);
+  }
+
+  /**
+  * @param {(string|string[])} [somebody=John Doe] - Somebody's name, or an array of names.
+  */
+  function sayHelloAllowsMultipleTypes(somebody) {
+    if (!somebody) {
+      somebody = 'John Doe';
+    } else if (Array.isArray(somebody)) {
+      somebody = somebody.join(', ');
+    }
+    alert('Hello ' + somebody);
+  }
 }
