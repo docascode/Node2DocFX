38f803ac1e70f4ca1b0bb4aa6b33405127b25284
Renze Yu <renzeyu@microsoft.com>
2017-02-06 15:37:15 +0800

    add sample using enum tag

--- test/fixtures/MyClass.js
+++ test/fixtures/MyClass.js
@@ -101,3 +101,16 @@ var MyNamespace = {
     /** documented as MyNamespace.bar */
     bar: 1
 };
+
+/**
+ * Enum for tri-state values.
+ * @readonly
+ * @enum {number}
+ */
+var triState = {
+    /** The true value */
+    TRUE: 1,
+    FALSE: -1,
+    /** @type {boolean} */
+    MAYBE: true
+};
